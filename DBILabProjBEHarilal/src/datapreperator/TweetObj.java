42da59e1b703aedcf623419fd00087ba72a8e1c1
Harilal <harilal.orunkara@mail.polimi.it>
2018-04-28 23:16:08 +0200

    Adding Data preprocessing related classes and jsons.

---
+++ DBILabProjBEHarilal/src/datapreperator/TweetObj.java
@@ -0,0 +1,16 @@
+package datapreperator;
+
+import java.util.List;
+
+public class TweetObj {
+	String created_at;
+	String id;
+	String tweet_text;
+	String user_id;
+	String in_reply_to_status_id;//"NULL" if no value
+	List<String> hash_tags;
+	List<String> user_mentions;
+	Integer retweet_count;
+	Integer favorite_count;
+	Boolean possibly_sensitive;//"NULL" if no value
+}
