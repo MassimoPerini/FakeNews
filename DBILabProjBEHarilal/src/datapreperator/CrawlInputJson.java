a003aad0edd4e84e0df1d135c6501acc163576fa
Harilal <harilal.orunkara@mail.polimi.it>
2018-04-28 23:26:27 +0200

    Removed wrongly pushed dir from repository

--- DBILabProjBEHarilal/src/datapreperator/CrawlInputJson.java
+++
@@ -1,9 +0,0 @@
-package datapreperator;
-
-import java.util.List;
-
-public class CrawlInputJson {
-	//{"current_ix": 0, "tweet_ids": ["911333326765441025", "890608763698200577"]}
-	public Integer current_ix;
-	public List<String> tweet_ids;
-}
