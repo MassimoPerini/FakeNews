99e6b25da7a1e7c9f73a7ac51c7aed6df725ce7b
alexbacce <alessio.baccelli@mail.polimi.it>
2018-04-30 17:32:44 +0200

    Conversion from JSON to csv, classification added

--- backend/sentiment_analysis.py
+++ backend/sentiment_analysis.py
@@ -12,25 +12,31 @@ natural_language_understanding = NaturalLanguageUnderstandingV1(
 with open('formatted_tweets.json') as json_data:
     d = json.load(json_data)
 
-file  = open("./response", "w")
+file  = open("./response.json", "w")
 file.write('{\n\t"tweets": [')
-#max_range = len(d["tweets"])
-max_range = 3
-for index in range(max_range):
+#max_range = len(d["tweets"]) #all tweets
+min_range = 1100
+max_range = 1600
+for index in range(min_range, max_range):
 
+    print(index, max_range)
     tweet_text = d["tweets"][index]["tweet_text"]
     tweet_id = d["tweets"][index]["id"]
-    file.write('\n\t\t{\n\t\t"tweet_id": ')
-    file.write(tweet_id)
-    file.write(',\n\t\t"tweet_analysis":\n\t\t\t')
 
-    response = natural_language_understanding.analyze(
-        text= tweet_text,
-        features=Features(entities=EntitiesOptions(), keywords=KeywordsOptions(sentiment=True, emotion=True)))
+    try:
+        response = natural_language_understanding.analyze(
+            text= tweet_text,
+            features=Features(entities=EntitiesOptions(), keywords=KeywordsOptions(sentiment=True, emotion=True)))
+        file.write('\n\t\t{\n\t\t"tweet_id": ')
+        file.write(tweet_id)
+        file.write(',\n\t\t"tweet_analysis":\n\t\t\t')
+        file.write(json.dumps(response))
+        file.write('\n\t\t}')
+        if index < max_range - 1:
+            file.write(',')
+    except:
+        print("an erorr occured")
+        continue
 
-    file.write(json.dumps(response))
-    file.write('\n\t\t}')
-    if index < max_range - 1:
-        file.write(',')
 file.write('\n\t]\n}')
 file.close()
