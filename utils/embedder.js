1d1fa2432c1217a7c334fd00929664f19b924352
Filippo Calzavara <filippo.calzavara@mail.polimi.it>
2018-07-03 19:57:51 +0200

    Fix repo

--- utils/embedder.js
+++ utils/embedder.js
@@ -2,20 +2,20 @@ var Twitter = require('twitter');
 var tokenTweet = require('./twitter');
 
 module.exports = {
-	getHTML: function(id,externalCallback) {
-		tokenTweet.getTwitterToken(function (accessToken){
+	getHTML: function(id, externalCallback) {
+		tokenTweet.getTwitterToken(function(accessToken) {
 			var client = new Twitter({
-				consumer_key: 'ncA4vA2bwaKuEaUVf6RJUfHON',
-				consumer_secret: '5SMe0zFj9HzfIRG3gVkUEbH6MprggAGnPC970dvwgQSKAdtpfu',
+				consumer_key: 'consumer_key',
+				consumer_secret: 'consumer_secret',
 				bearer_token: accessToken
-			});	
+			});
 
-			client.get('statuses/oembed',{url: 'https://twitter.com/Interior/status/'+id}, function(error, response, raw) {
-				if(error) throw error;
-			//console.log(response);
-			externalCallback(response["html"]);
-		});	
+			client.get('statuses/oembed', { url: 'https://twitter.com/Interior/status/' + id }, function(error, response, raw) {
+				if (error) throw error;
+				//console.log(response);
+				externalCallback(response["html"]);
+			});
 		})
 
-	}	
+	}
 }
